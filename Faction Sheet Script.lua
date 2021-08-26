0571eccc598b47860b6efb56d6707c408cc66e89
Tristan-Stevens <89171537+Tristan-Stevens@users.noreply.github.com>
2021-08-26 18:45:07 +1000

    Added loading/rewinding functionality

--- Faction Sheet Script.lua
+++ Faction Sheet Script.lua
@@ -1,29 +1,47 @@
-local factionBoxGUID = "" -- faction box GUID goes here!
+local factionBoxGUID = "BrawnenBox"
 
-function onLoad(save_state)
-  local params = {
-    label = "Play Faction",
-    click_function = "pickFaction",
-    function_owner = self,
-    position = {0.7, 0.1, -0.9},
-    width          = 360,
-    height         = 10,
-    font_size      = 60,
-    tooltip = "Loads this faction and all materials into your play area"
-  }
-  self.createButton(params)
+factionSetup = false
 
-  local params = {
-    label = "Setup AI",
-    click_function = "aiFaction",
-    function_owner = self,
-    position = {0.7, 0.1, 0.9},
-    width          = 360,
-    height         = 10,
-    font_size      = 60,
-    tooltip = "Does a limited setup of this faction for use as an AI player in solo and co-op"
+function onLoad(saveState)
+
+  if saveState ~= "" then
+    local saveData = JSON.decode(saveState)
+    factionSetup = saveData.factionSetup
+  end
+
+  if factionSetup ~= true then
+    local params = {
+      label = "Play Faction",
+      click_function = "pickFaction",
+      function_owner = self,
+      position = {0.7, 0.1, -0.9},
+      width          = 360,
+      height         = 10,
+      font_size      = 60,
+      tooltip = "Loads this faction and all materials into your play area"
+    }
+    self.createButton(params)
+
+    local params = {
+      label = "Setup AI",
+      click_function = "aiFaction",
+      function_owner = self,
+      position = {0.7, 0.1, 0.9},
+      width          = 360,
+      height         = 10,
+      font_size      = 60,
+      tooltip = "Does a limited setup of this faction for use as an AI player in solo and co-op"
+    }
+    self.createButton(params)
+  end
+  return JSON.encode(saveData)
+end
+
+function onSave()
+  local saveData = {
+    ["factionSetup"] = factionSetup
   }
-  self.createButton(params)
+  return JSON.encode(saveData)
 end
 
 function pickFaction(obj, player)
